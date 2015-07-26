935f76dabd77aa184cde10a248bf4958fdf3d3fd
Lorenzo Ritter <lorenzo.ritter@gmail.com>
2015-07-26 17:46:03 +0200

    uploading readme; cleaning up files; cleaning folder structure

--- Code/displayTopAirports.php
+++ Code/displayTopAirports.php
@@ -1,27 +1,11 @@
 <?php
-	$input = $_POST["numAirports"];
-	//$result = shell_exec('python /home/lorenzo/Documents/AmadeusDataChallenge/Code/topAirports.py');
-	#print_r(getenv());
-	#$path = sys.path;
-	#print $path;
-	#putenv('PYTHONPATH=x');
-	#print getenv();
+	$input = $_GET["numAirports"];
 	exec('python /home/lorenzo/Documents/AmadeusDataChallenge/Code/topAirports.py '.$input, $result);
-	#echo $result[5];
-	#echo 'hi 10';
-	//echo $values;
-	#$resultData = json_decode($result);
-	echo 'result: ';
-	var_dump($result);
-	echo '<br>resultData: ';
 	$resultData = json_decode($result[0], true);
-	#print_r(var_dump($resultData));
-	echo '<pre>'.print_r($resultData).'</pre>';
-	echo '<br>resultData[0]: ';
-	print_r($resultData[LHR][Arrivals]);
 	
-	echo "<table>";
-	echo "<th>Index</th><th>Arrivals</th><th>Name</th><th>City</th>";
+	echo "<html><center><h3>Top ".$input." Airports</h3>";
+	echo "<table border='1'>";
+	echo "<th>Code</th><th>Arrivals</th><th>Name</th><th>City</th>";
 	foreach ($resultData as $index=>$airport){
 		echo '<tr><td>';
 		print $index;
@@ -33,30 +17,5 @@
 		print $airport[City];
 		echo '</td></tr>';
 	}
-	echo '</table>';
-
-/*
-	echo '<table><tr><td>';
-	print $resultData[LHR][Arrivals];
-	echo '</td></tr><tr><td>';
-	echo '2323';
-	echo '</td></tr></table>';
-*/
-/*
-    #if (count($resultData->Arrivals)) {
-        // Open the table
-        echo "<table>";
-
-        // Cycle through the array
-        foreach ($resultData as $airport) {
-
-            // Output a row
-            echo "<tr>";
-            echo "<td>$airport</td>";
-            #echo "<td>$stand->JFK</td>";
-            echo "</tr>";
-    #    }
-
-        // Close the table
-        echo "</table>";*/	
+	echo '</table></html>';	
 ?>
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # datachallenge
+
+Solution to the Data Challenge by Lorenzo von Ritter
+July 26, 2015
+
+Folder structure
+----------------
+
+- **Code** contains the programmed Code
+  - **AmadeusDataChallenge.ipynb** iPython solution for all exercises
+  - **AmadeusDataChallenge-Sandbox.ipynb** Tests for iPython. All successful approaches were implemented in *AmadeusDataChallenge.ipynb*
+  - **AmadeusDataChallenge-withHDFStores.ipynb** Solution approach with HDFStores. This approach was not finished.
+  - **HDFStore.ipynb** Initializes the HDFStores for the HDFStores approach.
+  - **index.html** Start page for the web app. It allows to execute exercise 2 with a customized amount of top airports.
+  - **topAirports.py** Is called by *index.html* and fetches the top airports for the given input value. GeoBases does not work for the web app.
+  - **displayTopAirports.php** Displays the results of *topAirports.py* in a table.
+- **Files** contain the data for calculations and some result data
+  - **searchesExtract.csv** The first few lines of the searches file.
+  - **bookingsExtract.csv** The first few lines of the bookings file.
+  - **bookingsCleanExtract.csv** The first few lines of the clean bookings file. Spaces in front of the delimiter ('^') have been deleted by using *$sed 's/\s*^/^/g' bookingsExtract.csv > bookingsCleanExtract.csv*
+  - **bookedSearchesExtract.csv** The first few lines of the result of exercise 4. It shows the searches with an additional column indicating whether the search led to a booking.
